74ca39f69579cb9c98052b7aad09cd1f2f5f23b9
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-09-12 23:17:01 +0800

    build(deps): add lint staged

---
+++ lint-staged.config.mjs
@@ -0,0 +1,9 @@
+/**
+ * @filename: lint-staged.config.mjs
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  '*.{js,mjs,ts,tsx,json,css,scss,md,yml,yaml,vue,blade.php}': [
+    'prettier . --write',
+  ],
+};

e3051fc7a14966404b550fe5996d66363a3dd1b1
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-09-12 23:19:03 +0800

    build(deps): add prettier plugin tailwindcss

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,10 @@
 /**
  * @see https://prettier.io/docs/configuration
- * @type {import("prettier").Config}
+ * @type {import("prettier").Config & import('prettier-plugin-tailwindcss').PluginOptions}
  */
 const config = {
   singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
 };
 
 export default config;

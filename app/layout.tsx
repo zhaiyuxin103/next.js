e1423e0fd071112eb665d0eb634f3468fca1a6d6
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-09-13 10:02:29 +0800

    build(deps): add sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="h-screen">{children}</main>
+        <Toaster />
       </body>
     </html>
   );

84849254eeb2a14a0ad5ea0ab9a3f51e7d4c6ce0
YuXin Zhai <zhaiyuxin103@hotmail.com>
2025-09-13 10:04:11 +0800

    feat(libs): 初始化 prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '../app/generated/prisma/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
